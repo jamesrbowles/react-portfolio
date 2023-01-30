64cdd3d0dfcaf456bce2548164cfb3a4cc494ac7
James Bowles <jamesrbowles@gmail.com>
2023-01-30 21:47:43 +0100

    sphere positioning and size

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -15,35 +15,9 @@ const Landing = ({ setSelectedPage }) => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
   return (
-    <section
-      id="home"
-      className="md:flex md:justify-between md:items-center gap-16 py-10 mx-24 h-full"
-    >
-      {/*  IMAGE SECTION */}
-      <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
-        {isAboveMediumScreens ? (
-          /*  <div
-            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 
-            before:w-full before:max-w-[400px] md:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]"
-          > */
-          <Canvas className="bg-grey">
-            <OrbitControls enableZoom={false} />
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[-2, 5, 2]} intensity={1} />
-            <Sphere />
-          </Canvas>
-        ) : (
-          /*    </div> */
-          <Canvas className="">
-            <OrbitControls enableZoom={false} />
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[-2, 5, 2]} intensity={1} />
-            <Sphere />
-          </Canvas>
-        )}
-      </div>
+    <section id="home" className="py-10 mx-24 h-full relative">
       {/*  MAIN SECTION */}
-      <div className="z-30 basis-1/2 mt-12 md:mt-32">
+      <div className="md:flex md:flex-col md:justify-start z-30 mt-12 md:mt-32 w-3/4">
         <motion.div
           initial="hidden"
           whileInView="visible"
@@ -106,6 +80,15 @@ const Landing = ({ setSelectedPage }) => {
         >
           <SocialMediaIcons />
         </motion.div>
+      </div>
+      {/*  SPHERE SECTION */}
+      <div className="md:order-2 absolute right-8 bottom-8 h-80 w-2/5">
+        <Canvas className="-z-50">
+          <OrbitControls enableZoom={false} />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[-2, 5, 2]} intensity={1} />
+          <Sphere />
+        </Canvas>
       </div>
     </section>
   );
