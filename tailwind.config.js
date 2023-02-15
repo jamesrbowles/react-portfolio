fdffd2990c7f89eade98cba7044f588b5e08a374
James Bowles <jamesrbowles@gmail.com>
2023-02-15 18:20:24 +0100

    fixed background color bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,12 +19,12 @@ module.exports = {
         node: '#3c873a',
         tailwind: '#3490dc',
       },
-      backgroundImage: (theme) => ({
+      /*       backgroundImage: (theme) => ({
         'gradient-rainbow':
           'linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)',
         'gradient-rainblue':
           'linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)',
-      }),
+      }), */
       fontFamily: {
         opensans: ['Open Sans', 'sans-serif'],
         leagueSpartan: ['League Spartan', 'serif'],
